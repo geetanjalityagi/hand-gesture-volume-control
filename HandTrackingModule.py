8c3c6f8d9b1b63b97d238b6de1a3cf5a31289d30
geetanjalityagi <geetanjalityagi1057@gmail.com>
2026-08-01 15:36:12 +0530

    Add hand landmarks

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,5 +1,10 @@
 import cv2
 import time
+import mediapipe as mp
+
+mpDraw = mp.solutions.drawing_utils
+mphands = mp.solutions.hands
+hand = mphands.Hands()
 
 cap = cv2.VideoCapture(1)
 
@@ -8,11 +13,18 @@ while True:
 
     ok, frame = cap.read()
 
-    ctime = time.time()
-    fps = 1/(ctime - ptime)
-    ptime = 0
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hand.process(img)
 
-    cv2.putText(frame, f"FPS: {fps: .2f}", (40, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            mpDraw.draw_landmarks(frame, handLms, mphands.HAND_CONNECTIONS)
+
+    ctime = time.time()
+    fps = 1 / (ctime - ptime) if ptime != 0 else 0
+    ptime = ctime
+
+    cv2.putText(frame, f"FPS: {fps:.2f}", (40, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
     cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) == ord('q'):
