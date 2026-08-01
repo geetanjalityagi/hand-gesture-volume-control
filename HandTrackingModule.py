72fb123d422267198d022c7c387bfd7ca41c67d4
geetanjalityagi <geetanjalityagi1057@gmail.com>
2026-08-02 00:04:24 +0530

    Update module

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 import mediapipe as mp
 
 
@@ -41,37 +40,3 @@ class handDetector():
                     lmlist.append([id, cx, cy])
 
         return lmlist
-
-
-
-
-def main():
-    cap = cv2.VideoCapture(1)
-
-    ptime = 0
-    ctime = 0
-
-    detector = handDetector()
-    while True:
-
-        ok, frame = cap.read()
-
-        frame = detector.findhands(frame)
-        lmlist = detector.findpoints(frame)
-
-        ctime = time.time()
-        fps = 1 / (ctime - ptime) if ptime != 0 else 0
-        ptime = ctime
-        
-        cv2.putText(frame, f"FPS: {fps:.2f}", (40, 70), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 1)
-        cv2.imshow("Frame", frame)
-        
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
-
